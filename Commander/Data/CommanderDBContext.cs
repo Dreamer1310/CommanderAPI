76b8bfc896cf73fc10738dd1a65b5f277a6f98d4
Dreamer <ltsir17@freeuni.edu.ge>
2022-05-30 20:47:12 +0400

    Initial commit

---
+++ Commander/Data/CommanderDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class CommanderDBContext : DbContext
+    {
+        public CommanderDBContext(DbContextOptions<CommanderDBContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+    }
+}
---
+++ Commander/Dtos/CommandReadDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Dtos
+{
+    public class CommandReadDto
+    {
+        public Int32 Id { get; set; }
+        public String HowTo { get; set; }
+        public String Line { get; set; }
+    }
+}
